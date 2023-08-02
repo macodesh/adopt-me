dffde20b9a752f95e74aef9faeb37d6b60c57b7f
Marlon Couto <devpatola@proton.me>
2023-08-01 22:55:00 -0300

    Adiciona v8 para cobertura de testes

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'happy-dom',
-    coverage: {
-      provider: 'istanbul',
-    }
+    environment: 'happy-dom'
   }
 })
